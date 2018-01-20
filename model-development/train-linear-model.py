7dfe745f197c9682d0e44c92b12008a043d9a173
The-Ben <ben@theaarons.com>
2018-01-20 12:44:14 -0800

    remove .ipynb, added regression training script

---
+++ model-development/train-linear-model.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import pickle
+
+train_data = pd.read_csv('cases.csv')
+lin_reg_model = LinearRegression()
+lin_reg_model.fit(train_data[['Q1','Q2']], train_data['A'])
+
+pickle.dump(lin_reg_model, open('model-linear.pkl', 'wb'))
